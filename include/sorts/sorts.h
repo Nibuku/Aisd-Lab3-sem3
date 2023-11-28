a26263e50634b27fe45c9c61dca2b62e57af741c
Вероника Будюк <veronika.budyuk2004@gmail.com>
2023-11-28 16:31:06 +0400

    done point 1 with 3 sorts

--- include/sorts/sorts.h
+++ include/sorts/sorts.h
@@ -29,3 +29,59 @@ stats insertion_sort(std::vector<int>& arr) {
     }
     return s;
 }
+
+stats coctail_sort(std::vector<int>& arr) {
+    int size = arr.size();
+    stats s;
+    if (size < 2)
+        return s;
+    int low = 0;
+    int up = size - 1;
+    int last=-1;
+    while (low < up) {
+        last = -1;
+        for (int i = low; i < up; ++i) {
+            ++s.comparison_count;
+            if (arr[i] > arr[i + 1]) {
+                swap(arr[i], arr[i + 1]);
+                last = i;
+                ++s.copy_count;
+            }
+        }
+        up = last;
+        if (last == -1)
+            break;
+        for (int i = up - 1; i >= low; --i) {
+            ++s.comparison_count;
+            if (arr[i] > arr[i + 1]) {
+                swap(arr[i], arr[i + 1]);
+                last = i;
+                ++s.copy_count;
+            }
+        }
+        low = last + 1;
+    }
+    return s;
+}
+
+stats comb_sort(std::vector<int>& arr) {
+    stats s;
+    int size = arr.size();
+    if (size < 2)
+        return s;
+    int step = size;
+    while (step >= 1) {
+        int swaps = 0;
+        for (int i = 0; i < size-step; ++i) {
+            ++s.comparison_count;
+            if (arr[i] > arr[i + step]) {
+                swap(arr[i], arr[i + step]);
+                ++s.copy_count;
+                ++swaps;
+            }
+        }
+        step = step - 1;
+        if (swaps == 0 && step==1)
+            break;
+    }return s;
+}
