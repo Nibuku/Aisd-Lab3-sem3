00f8b2b8d8549e3f05b59d7cb94a3b90babcc4ad
Вероника Будюк <veronika.budyuk2004@gmail.com>
2023-11-30 22:55:41 +0400

    done additional task with iterators

--- include/sorts/sorts.h
+++ include/sorts/sorts.h
@@ -5,6 +5,7 @@
 #include <random>
 #include <vector>
 
+
 using namespace std;
 
 struct stats {
@@ -59,7 +60,7 @@ stats coctail_sort(std::vector<int>& arr) {
                 ++s.copy_count;
             }
         }
-        low = last + 1;
+        low = last;
     }
     return s;
 }
@@ -70,18 +71,117 @@ stats comb_sort(std::vector<int>& arr) {
     if (size < 2)
         return s;
     int step = size;
-    while (step >= 1) {
-        int swaps = 0;
+    bool swaps = true;
+    while (step >= 1|| swaps ==true) {
+        swaps = false;
         for (int i = 0; i < size-step; ++i) {
             ++s.comparison_count;
             if (arr[i] > arr[i + step]) {
                 swap(arr[i], arr[i + step]);
                 ++s.copy_count;
-                ++swaps;
+                swaps=true;
             }
         }
         step = step - 1;
-        if (swaps == 0 && step==1)
+
+    }return s;
+}
+
+vector<int> random_vector(int size, int min, int max) {
+    vector<int> arr;
+    for (int i = 0; i < size; ++i) {
+        arr.push_back(rand() % (max - min + 1) + min);
+    }
+    return arr;
+}
+
+vector<int> normal_vector(int size) {
+    vector<int> arr;
+    for (int i = 0; i < size; ++i) {
+        arr.push_back(i);
+    }
+    return arr;
+}
+
+vector<int> reverse_vector(int size) {
+    vector<int> arr;
+    for (int i =size-1 ; i >=0; --i) {
+        arr.push_back(i);
+    }
+    return arr;
+}
+
+template <typename Iterator> 
+stats insertion_sort(Iterator begin, Iterator end) {
+    Iterator i, j;
+    stats s;
+    for ( i = begin; i != end; ++i) {
+        for (j = i; j !=begin; --j) {
+            ++s.comparison_count;
+            if (*j < *(j - 1)) {
+                swap(*j, *(j - 1));;
+                ++s.copy_count;
+            }
+            else break;
+        }
+    } 
+    return s;
+}
+
+template <typename Iterator>
+stats coctail_sort(Iterator begin, Iterator end) {
+    Iterator i, j;
+    stats s;
+
+    Iterator low = begin;
+    Iterator up = (end-1);
+    Iterator last = end;
+    while (low < up) {
+        last = end;
+        for ( i = begin; i < up; ++i) {
+            ++s.comparison_count;
+            if (*i > *(i + 1)) {
+                std::swap(*i, *(i + 1));
+                last = i;
+                ++s.copy_count;
+            }
+        }
+        up = last;
+        if (last ==end)
             break;
+        for (i = up-1; i > low; --i) {
+            ++s.comparison_count;
+            if (*i > *(i + 1)) {
+                std::swap(*i, *(i + 1));
+                last = i;
+                ++s.copy_count;
+            }
+        }
+        low = last;
+    }
+    return s;
+}
+
+template <typename Iterator>
+stats comb_sort(Iterator begin, Iterator end) {
+    stats s;
+    Iterator i, j;
+    int size = end - begin;
+    if (size < 2)
+        return s;
+    int step = size;
+    bool swaps = true;
+    while (step >= 1 || swaps == true) {
+        swaps = false;
+        for ( i = begin; i < (end - step); ++i) {
+            ++s.comparison_count;
+            if (*i > *(i + step)) {
+                swap(*i, *(i + step));
+                ++s.copy_count;
+                swaps = true;
+            }
+        }
+        step = step - 1;
+
     }return s;
 }
--- tests/sorting_tests.cc
+++ tests/sorting_tests.cc
@@ -2,6 +2,7 @@
 
 #include <sorts/sorts.h>
 
+
 TEST(sorts, insertionSort) {
 	vector<int> v = { 6,5,9,8,1,0,2,4,3,7 };
 	vector<int> v_sorted = { 0,1,2,3,4,5,6,7,8,9 };
@@ -86,4 +87,232 @@ TEST(sorts, combSortIdenticalNumbers) {
 	EXPECT_TRUE(v_sorted == v);
 }
 
+TEST(vectors, randomVector) {
+	srand(42);
+	vector<int> v = random_vector(20, 40, 100);
+	for (int i = 0; i < v.size(); ++i)
+		cout << v[i]<<" ";
+	cout << endl;
+	EXPECT_TRUE(20 == v.size());
+}
+
+TEST(vectors, normalVector) {
+	vector<int> v = normal_vector(20);
+	for (int i = 0; i < v.size(); ++i)
+		cout << v[i] << " ";
+	cout << endl;
+	EXPECT_TRUE(20 == v.size());
+}
+
+TEST(vectors, reverseVector) {
+	vector<int> v = reverse_vector(20);
+	for (int i = 0; i < v.size(); ++i)
+		cout << v[i] << " ";
+	cout << endl;
+	EXPECT_TRUE(20 == v.size());
+}
+
+TEST(sortsIterator, InsertionSortWithIterators) {
+	vector<int> v = reverse_vector(100);
+	vector<int> v_sorted = normal_vector(100);
+	stats statistic = insertion_sort(v.begin(), v.end());
+	for (int i = 0; i < v.size(); ++i)
+		cout << v[i] << " ";
+	cout << endl;
+	cout << "Comparison_count:" << statistic.comparison_count << " copy_count:" << statistic.copy_count << endl;
+	EXPECT_TRUE(v_sorted == v);
+}
+
+TEST(sortsIterator, CoctailSortWithIterators) {
+	vector<int> v = reverse_vector(100);
+	vector<int> v_sorted = normal_vector(100);
+	stats statistic = coctail_sort(v.begin(), v.end());
+	for (int i = 0; i < v.size(); ++i)
+		cout << v[i] << " ";
+	cout << endl;
+	cout << "Comparison_count:" << statistic.comparison_count << " copy_count:" << statistic.copy_count << endl;
+	EXPECT_TRUE(v_sorted == v);
+}
+
+TEST(sortsIterator, CombSortWithIterators) {
+	vector<int> v = reverse_vector(100);
+	vector<int> v_sorted = normal_vector(100);
+	stats statistic = comb_sort(v.begin(), v.end());
+	for (int i = 0; i < v.size(); ++i)
+		cout << v[i] << " ";
+	cout << endl;
+	cout << "Comparison_count:" << statistic.comparison_count << " copy_count:" << statistic.copy_count << endl;
+	EXPECT_TRUE(v_sorted == v);
+}
+
+/*TEST(vectors, randonSize1000) {
+	srand(42);
+	size_t sum_comparison = 0, sum_copy = 0;
+	for (size_t i = 0; i < 100; ++i) {
+		vector<int> v = random_vector(1000, 0, 500);
+		stats counter = insertion_sort(v);
+		sum_comparison += counter.comparison_count;
+		sum_copy += counter.copy_count;
+	}
+	cout << "size: " << 1000 << endl;
+	cout << "Average comparison quantity: " << sum_comparison / 100 << endl;
+	cout << "Average copy quantity: " << sum_copy / 100 << endl;
+
+}
+
+TEST(vectors, randonSize2000) {
+	size_t sum_comparison = 0, sum_copy = 0;
+	for (size_t i = 0; i < 100; ++i) {
+		vector<int> v = random_vector(2000, 0, 500);
+		stats counter = insertion_sort(v);
+		sum_comparison += counter.comparison_count;
+		sum_copy += counter.copy_count;
+	}
+	cout << "size: " << 2000 << endl;
+	cout << "Average comparison quantity: " << sum_comparison / 100 << endl;
+	cout << "Average copy quantity: " << sum_copy / 100 << endl;
+}
+
+TEST(vectors, randonSize3000) {
+	size_t sum_comparison = 0, sum_copy = 0;
+	for (size_t i = 0; i < 100; ++i) {
+		vector<int> v = random_vector(3000, 0, 500);
+		stats counter = insertion_sort(v);
+		sum_comparison += counter.comparison_count;
+		sum_copy += counter.copy_count;
+	}
+	cout << "size: " << 3000 << endl;
+	cout << "Average comparison quantity: " << sum_comparison / 100 << endl;
+	cout << "Average copy quantity: " << sum_copy / 100 << endl;
+}
+
+TEST(vectors, randonSize4000) {
+	size_t sum_comparison = 0, sum_copy = 0;
+	for (size_t i = 0; i < 100; ++i) {
+		vector<int> v = random_vector(4000, 0, 500);
+		stats counter = insertion_sort(v);
+		sum_comparison += counter.comparison_count;
+		sum_copy += counter.copy_count;
+	}
+	cout << "size: " << 4000 << endl;
+	cout << "Average comparison quantity: " << sum_comparison / 100 << endl;
+	cout << "Average copy quantity: " << sum_copy / 100 << endl;
+}
+
+TEST(vectors, randonSize5000) {
+	size_t sum_comparison = 0, sum_copy = 0;
+	for (size_t i = 0; i < 100; ++i) {
+		vector<int> v = random_vector(5000, 0, 500);
+		stats counter = insertion_sort(v);
+		sum_comparison += counter.comparison_count;
+		sum_copy += counter.copy_count;
+	}
+	cout << "size: " << 5000 << endl;
+	cout << "Average comparison quantity: " << sum_comparison / 100 << endl;
+	cout << "Average copy quantity: " << sum_copy / 100 << endl;
+}
+
+TEST(vectors, randonSize6000) {
+	size_t sum_comparison = 0, sum_copy = 0;
+	for (size_t i = 0; i < 100; ++i) {
+		vector<int> v = random_vector(6000, 0, 500);
+		stats counter = insertion_sort(v);
+		sum_comparison += counter.comparison_count;
+		sum_copy += counter.copy_count;
+	}
+	cout << "size: " << 6000 << endl;
+	cout << "Average comparison quantity: " << sum_comparison / 100 << endl;
+	cout << "Average copy quantity: " << sum_copy / 100 << endl;
+}
+
+TEST(vectors, randonSize7000) {
+	size_t sum_comparison = 0, sum_copy = 0;
+	for (size_t i = 0; i < 100; ++i) {
+		vector<int> v = random_vector(7000, 0, 500);
+		stats counter = insertion_sort(v);
+		sum_comparison += counter.comparison_count;
+		sum_copy += counter.copy_count;
+	}
+	cout << "size: " << 7000 << endl;
+	cout << "Average comparison quantity: " << sum_comparison / 100 << endl;
+	cout << "Average copy quantity: " << sum_copy / 100 << endl;
+}
+
+TEST(vectors, randonSize8000) {
+	size_t sum_comparison = 0, sum_copy = 0;
+	for (size_t i = 0; i < 100; ++i) {
+		vector<int> v = random_vector(8000, 0, 500);
+		stats counter = insertion_sort(v);
+		sum_comparison += counter.comparison_count;
+		sum_copy += counter.copy_count;
+	}
+	cout << "size: " << 8000 << endl;
+	cout << "Average comparison quantity: " << sum_comparison / 100 << endl;
+	cout << "Average copy quantity: " << sum_copy / 100 << endl;
+}
+
+TEST(vectors, randonSize9000) {
+	size_t sum_comparison = 0, sum_copy = 0;
+	for (size_t i = 0; i < 100; ++i) {
+		vector<int> v = random_vector(9000, 0, 500);
+		stats counter = insertion_sort(v);
+		sum_comparison += counter.comparison_count;
+		sum_copy += counter.copy_count;
+	}
+	cout << "size: " << 9000 << endl;
+	cout << "Average comparison quantity: " << sum_comparison / 100 << endl;
+	cout << "Average copy quantity: " << sum_copy / 100 << endl;
+}
+
+TEST(vectors, randonSize10000) {
+	size_t sum_comparison = 0, sum_copy = 0;
+	for (size_t i = 0; i < 100; ++i) {
+		vector<int> v = random_vector(10000, 0, 500);
+		stats counter = insertion_sort(v);
+		sum_comparison += counter.comparison_count;
+		sum_copy += counter.copy_count;
+	}
+	cout << "size: " << 10000 << endl;
+	cout << "Average comparison quantity: " << sum_comparison / 100 << endl;
+	cout << "Average copy quantity: " << sum_copy / 100 << endl;
+}
+
+TEST(vectors, randonSize25000) {
+	size_t sum_comparison = 0, sum_copy = 0;
+	for (size_t i = 0; i < 100; ++i) {
+		vector<int> v = random_vector(25000, 0, 500);
+		stats counter = insertion_sort(v);
+		sum_comparison += counter.comparison_count;
+		sum_copy += counter.copy_count;
+	}
+	cout << "size: " << 25000 << endl;
+	cout << "Average comparison quantity: " << sum_comparison / 100 << endl;
+	cout << "Average copy quantity: " << sum_copy / 100 << endl;
+}
+
+TEST(vectors, randonSize50000) {
+	size_t sum_comparison = 0, sum_copy = 0;
+	for (size_t i = 0; i < 100; ++i) {
+		vector<int> v = random_vector(50000, 0, 500);
+		stats counter = insertion_sort(v);
+		sum_comparison += counter.comparison_count;
+		sum_copy += counter.copy_count;
+	}
+	cout << "size: " << 50000 << endl;
+	cout << "Average comparison quantity: " << sum_comparison / 100 << endl;
+	cout << "Average copy quantity: " << sum_copy / 100 << endl;
+}
+
+TEST(vectors, randonSize100000) {
+	size_t sum_comparison = 0, sum_copy = 0;
+	for (size_t i = 0; i < 100; ++i) {
+		vector<int> v = random_vector(100000, 0, 500);
+		stats counter = insertion_sort(v);
+		sum_comparison += counter.comparison_count;
+		sum_copy += counter.copy_count;
+	}
+	cout << "size: " << 100000 << endl;
+	cout << "Average comparison quantity: " << sum_comparison / 100 << endl;
+	cout << "Average copy quantity: " << sum_copy / 100 << endl;
+}*/
 
